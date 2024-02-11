6b89b50e5d4d95c0c6913a7d6d846acd3fa28b72
Peter Kwok <peter.kwok94@gmail.com>
2024-02-11 00:08:48 -0500

    feat: outline
    feat: random number in range

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+# generate random number 
+min=1
+max=1000
+RANDOM_NUMBER=$(($RANDOM%($max-$min+1)+$min))
+# read username
+# if username exists 
+# display welcome back 
+# else display welcome first time 
+# display game explanation
+# run game 
+
+# read user guess 
+# if guess > target
+# display hint
+# increment tries
+# try again
+# else if guess < target
+# display hint
+# increment tries
+# try again
+# else if guess = target
+# disiplay winning summary 
+# else display invalid input
+
